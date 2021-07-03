51a33070720c5ae3dff5b1bc6350be52c77f8651
Abhi3532 <86361986+Abhi3532@users.noreply.github.com>
2021-07-03 07:36:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCT5mUIyXWHMmjztdJHwSFJmeSitdpPwMA",
+    authDomain: "mobile-mania-a5c22.firebaseapp.com",
+    projectId: "mobile-mania-a5c22",
+    storageBucket: "mobile-mania-a5c22.appspot.com",
+    messagingSenderId: "1022934589746",
+    appId: "1:1022934589746:web:95f7bada9bfd809863772b",
+    measurementId: "G-L4RE2YBKVY"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
